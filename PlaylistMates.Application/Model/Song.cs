ae6a81b90f36120e92eea9246a53d386e2335897
BenjaminSobhian <benjamin@sobhian.at>
2022-12-21 10:13:08 +0100

    Add IReadOnlyList to Song

--- PlaylistMates.Application/Model/Song.cs
+++ PlaylistMates.Application/Model/Song.cs
@@ -9,6 +9,7 @@ namespace PlaylistMates.Application.Model
 
     public class Song : IEntity<string>
     {
+        // TODO: check whether two constructors are needed
         public Song(string isrcCode, string title, DateTime releaseDate, int durationInMillis, ICollection<Artist> artists, ICollection<Platform> platforms)
         {
             // id is the isrc code of the song
@@ -18,18 +19,6 @@ namespace PlaylistMates.Application.Model
             DurationInMillis = durationInMillis;
             Artists = artists;
             Platforms = platforms;
-        }
-
-        public Song(string isrcCode, string title, DateTime releaseDate, int durationInMillis, ICollection<Artist> artists, ICollection<Platform> platforms, ICollection<SongCollection> songCollections)
-        {
-            // id is the isrc code of the song
-            IsrcCode = isrcCode;
-            Titel = title;
-            ReleaseDate = releaseDate;
-            DurationInMillis = durationInMillis;
-            Artists = artists;
-            Platforms = platforms;
-            SongCollections = songCollections;
         }
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
@@ -44,8 +33,13 @@ namespace PlaylistMates.Application.Model
         public int DurationInMillis { get; set; }   
         public ICollection<Artist> Artists { get; set; }
         public ICollection<Platform> Platforms { get; set; }
-
-        public ICollection<SongCollection>? SongCollections { get; set; }
+        protected List<SongCollection> _songCollections = new();
+        public IReadOnlyCollection<SongCollection> SongCollections => _songCollections;
         public string Id => IsrcCode;
+
+        public void AddSongCollection(SongCollection songCollection)
+        {
+            _songCollections.Add(songCollection);
+        }
     }
 }
