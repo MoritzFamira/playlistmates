299aa1284a20e0cf7881be6a6ed3ad1b531907b0
BenjaminSobhian <benjamin@sobhian.at>
2022-12-21 10:13:29 +0100

    Add protected default constructor to Platform

--- PlaylistMates.Application/Model/Platform.cs
+++ PlaylistMates.Application/Model/Platform.cs
@@ -12,7 +12,10 @@ namespace PlaylistMates.Application.Model
         {
             Id = id;
             Name= name;
-        } 
+        }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        protected Platform() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public int Id { get; set; }
         public string Name { get; set; }
         // TODO: think of more fields
