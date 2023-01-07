c74def5d5e43b8b476184ae64f1ec88395ba6a80
BenjaminSobhian <benjamin@sobhian.at>
2023-01-07 18:23:05 +0100

    Added Action to LogItem

--- PlaylistMates.Application/Model/LogItem.cs
+++ PlaylistMates.Application/Model/LogItem.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlaylistMates.Application.Model
 {
-    public class LogItem
+    public class LogItem : IEntity<int>
     {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         protected LogItem() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         
-        public LogItem(int id, DateTime timeStamp, Account account)
+        public LogItem(int id, DateTime timeStamp, Account account, Action action)
         {
             Id = id;
             TimeStamp = timeStamp;
             Account = account;
+            Action = action;
         }
+        [Key]
         public int Id { get; set; }
         public DateTime TimeStamp { get; set; }
         public Account Account { get; set; }
+        public Action Action { get; set; }
     }
 }
