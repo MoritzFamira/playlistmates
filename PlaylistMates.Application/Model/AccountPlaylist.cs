557835ca022a52957f683893ef01a22a2f4e1be8
BenjaminSobhian <benjamin@sobhian.at>
2023-01-07 18:24:04 +0100

    Added explicit m:n between Account and Playlist

---
+++ PlaylistMates.Application/Model/AccountPlaylist.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlaylistMates.Application.Model
+{
+    public class AccountPlaylist
+    {
+
+        /*
+         * This entity represents the m:n relationship between Account and Playlist.
+         * It is an explicit relationship since the extra column "Role" needed could not be implemented otherwise.
+        */
+        public AccountPlaylist(int playlist, int account, PlaylistRole role)
+        {
+            PlaylistId = playlist;
+            AccountId = account;
+            Role = role;
+        }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        protected AccountPlaylist() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        [Column(Order = 1)]
+        [ForeignKey("Playlist")]
+        public int PlaylistId { private set; get; }
+        [Column(Order = 2)]
+        [ForeignKey("Account")]
+        public int AccountId { private set; get; }
+
+        public PlaylistRole Role { get; set; }
+    }
+}
--- PlaylistMates.Application/Model/Playlist.cs
+++ PlaylistMates.Application/Model/Playlist.cs
@@ -8,16 +8,18 @@ namespace PlaylistMates.Application.Model
 {
     public class Playlist : SongCollection
     {
-        public Playlist(string description, bool isPublic)
+        public Playlist(string description, bool isPublic, ICollection<AccountPlaylist> accountPlaylists)
         {
             Description = description;
             IsPublic = isPublic;
+            AccountPlaylists = accountPlaylists;
         }
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         protected Playlist() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public string Description { get; set; }
         public bool IsPublic { get; set; }
+        public ICollection<AccountPlaylist> AccountPlaylists { get; set; }
         
         // TODO: way to store who playlist is shared with / who faved it (m:n) (waiting for account)
     }
