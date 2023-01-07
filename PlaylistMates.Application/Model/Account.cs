a7523b39a7d9a45f29c4d290811a495c8a44e102
BenjaminSobhian <benjamin@sobhian.at>
2023-01-07 18:22:47 +0100

    Remove Email Value object and started with AccountPlaylists

--- PlaylistMates.Application/Model/Account.cs
+++ PlaylistMates.Application/Model/Account.cs
@@ -8,25 +8,30 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlaylistMates.Application.Model
 {
-    public class Account : IEntity<Email>
+    public class Account : IEntity<string>
     {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         protected Account() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-        public Account(Email email, string accountName, string salt, ICollection<Platform> platforms)
+        public Account(string email, string accountName, string salt, ICollection<Platform> platforms, ICollection<Playlist> playlists, ICollection<AccountPlaylist> accountPlaylists)
         {
             Email = email;
             AccountName = accountName;
             Salt = salt;
             Platforms = platforms;
+            Playlists = playlists;
+            AccountPlaylists = accountPlaylists;
         }
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public Email Email { get; set; }
+        public string Email { get; private set; }
         public string AccountName { get; set; }
         public string Salt { get; set; }
         public ICollection<Platform> Platforms { get; set; }
-        public Email Id => Email;
+        public ICollection<Playlist> Playlists { get; set; }
+        public string Id => Email;
+        public ICollection<AccountPlaylist> AccountPlaylists { get; set; }
+
     }
 }
