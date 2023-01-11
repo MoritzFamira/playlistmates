5a3e8e97e0ce848225ab781a78c931615b8317d8
BenjaminSobhian <benjamin@sobhian.at>
2023-01-11 11:31:20 +0100

    Remove id from songcollection constructor

--- PlaylistMates.Application/Model/SongCollection.cs
+++ PlaylistMates.Application/Model/SongCollection.cs
@@ -8,16 +8,15 @@ namespace PlaylistMates.Application.Model
 {
     public class SongCollection : IEntity<int>
     {
-        public SongCollection(int id, string title, DateTime creationDate)
+        public SongCollection(string title, DateTime creationDate)
         {
-            Id = id;
             Title = title;
             CreationDate = creationDate;
         }
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         protected SongCollection() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        public int Id { get; set; }
+        public int Id { get; private set; }
         public string Title { get; set; }
         public DateTime CreationDate { get; set; }
         
