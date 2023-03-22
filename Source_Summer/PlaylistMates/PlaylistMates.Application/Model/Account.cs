d3023b50cad05222080aa0f3e9d55b7d5d03f4bf
BenjaminSobhian <benjamin@sobhian.at>
2023-03-22 18:52:36 +0100

    Add HashedPassword and moved GenerateSalt()

--- Source_Summer/PlaylistMates/PlaylistMates.Application/Model/Account.cs
+++ Source_Summer/PlaylistMates/PlaylistMates.Application/Model/Account.cs
@@ -7,7 +7,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
-
+using System.Text.Json.Serialization;
 
 namespace PlaylistMates.Application.Model
 {
@@ -18,31 +18,24 @@ namespace PlaylistMates.Application.Model
         protected Account() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-        public Account(string email, string accountName)
+        public Account(string email, string accountName, string salt, string hashedPassword)
         {
             Email = email;
             AccountName = accountName;
-            Salt = GenerateSalt();
+            Salt = salt;
+            HashedPassword = hashedPassword;
         }
 
         public int Id { get; private set; }
         public string Email { get; private set; }
         public string AccountName { get; set; }
-        public string Salt { get; private set; }
+        [JsonIgnore]
+        public string HashedPassword { get; set; } 
+        public string Salt { get; private set; } 
         public List<Platform> Platforms { get; } = new();
         public List<Playlist> Playlists { get; } = new();
-        public List<AccountPlaylist> AccountPlaylists { get;  } = new();
+        public List<AccountPlaylist> AccountPlaylists { get; } = new();
         public List<AccountPlatforms> AccountPlatforms { get; } = new();
 
-
-        private string GenerateSalt()
-        {
-            byte[] salt = new byte[16];
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                rng.GetBytes(salt);
-            }
-            return (Convert.ToBase64String(salt));
-        }
     }
 }
