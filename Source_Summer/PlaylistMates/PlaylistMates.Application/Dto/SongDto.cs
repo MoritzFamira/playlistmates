3a3a7312a09f34fc3b479b2cac9d229b3191e4c1
BenjaminSobhian <benjamin@sobhian.at>
2023-04-14 08:14:44 +0200

    Add Validation Annotations

--- Source_Summer/PlaylistMates/PlaylistMates.Application/Dto/SongDto.cs
+++ Source_Summer/PlaylistMates/PlaylistMates.Application/Dto/SongDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using PlaylistMates.Application.Model;
 using PlaylistMates.Application.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,12 +7,30 @@ namespace PlaylistMates.Application.Dto
 {
     public record SongDto
     (
+        [Required]
         Guid Guid,
+
+        [Required]
+        [StringLength(12, MinimumLength = 12, ErrorMessage = "ISRC code must be 12 characters long.")]
         string IsrcCode,
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Title must be at most 100 characters long.")]
         string Titel,
+
+        [Required]
         DateTime ReleaseDate,
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Duration must be greater than 0 milliseconds.")]
         int DurationInMillis,
+
+        [Required]
+        [MinLength(1, ErrorMessage = "At least one artist is required.")]
         List<Artist> Artists,
+
+        [Required]
+        [MinLength(1, ErrorMessage = "At least one SongCollection Guid is required.")]
         List<Guid> SongCollectionGuids
     ) : IValidatableObject
     {
