a7a1fd97e379c9a26b6cc92fb868a379232b0ad4
Moritz Jakob Famira <famiramoritz@gmail.com>
2023-06-12 20:55:16 +0200

    add client

---
+++ Source_Summer/PlaylistMates/playlistmates.client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PlaylistList from './components/List';
+
+function App() {
+  return (
+    <div className="App">
+      <PlaylistList/>
+    </div>
+  );
+}
+
+export default App;
