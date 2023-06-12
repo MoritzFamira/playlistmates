8d9c5bdfbc0a73a720744249b337922acda7ed75
Moritz Jakob Famira <famiramoritz@gmail.com>
2023-06-12 19:57:43 +0200

    start frontend

--- Source_Summer/PlaylistMates/PlaylistMates.Webapi/Controllers/PlaylistController.cs
+++ Source_Summer/PlaylistMates/PlaylistMates.Webapi/Controllers/PlaylistController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Security.Claims;
+using Castle.Core.Internal;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PlaylistMates.Webapi.Services;
 using PlaylistMates.Application.Infrastructure;
 using PlaylistMates.Application.Model;
@@ -44,7 +46,39 @@ namespace PlaylistMates.Webapi.Controllers
             _logger.LogInformation("Non-user got playlist with id {Id} at {DT}",playlistId,DateTime.UtcNow.ToLongTimeString());
             return playlist;
         }
+        
+        [HttpGet("/api/Playlist/byUser/{email}")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<ActionResult<List<Playlist>>> GetPlaylistsByUser(String email)
+        {
+            _logger.LogDebug(email);
+            var loggedInUserEmail = HttpContext.User.FindFirst(ClaimTypes.Name)?.Value;
+            
+            if(loggedInUserEmail == null)
+            {
+                return Forbid(); // if there's no email claim in the token
+            }
 
+            if (!string.Equals(loggedInUserEmail, email, StringComparison.OrdinalIgnoreCase))
+            {
+                return Forbid(); // if the logged in user email does not match the requested user email
+            }
+
+            List<Playlist> playlists = _context.Accounts
+                .Include(a => a.AccountPlaylists)
+                .ThenInclude(ap => ap.Playlist)
+                .Where(a => a.Email == email)
+                .SelectMany(a => a.AccountPlaylists
+                    .Select(ap => ap.Playlist)).ToList();
+            Console.WriteLine(playlists.ToString());
+            if (playlists.IsNullOrEmpty()) { return Forbid(); }
+            
+            return playlists;
+        }
         //[HttpPost]
         //[ProducesResponseType(StatusCodes.Status201Created)]
         //[ProducesResponseType(StatusCodes.Status400BadRequest)]
