5967f7e80bb9cbe8ef4eb3de2a2aa3c64d5d9803
BenjaminSobhian <benjamin@sobhian.at>
2023-06-12 23:15:14 +0200

    Fox

--- Source_Summer/PlaylistMates/PlaylistMates.Webapi/Program.cs
+++ Source_Summer/PlaylistMates/PlaylistMates.Webapi/Program.cs
@@ -63,13 +63,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseCors(x => x
     .AllowAnyMethod()
     .AllowAnyHeader()
     .SetIsOriginAllowed(origin => true) // allow any origin
     .AllowCredentials()); // allow credentials
+
+app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
