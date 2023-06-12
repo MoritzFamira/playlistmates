6977de116dc2a176d51f4fd6f2bfab0a425dc889
BenjaminSobhian <benjamin@sobhian.at>
2023-06-12 23:08:24 +0200

    Enable to disable cors

--- Source_Summer/PlaylistMates/PlaylistMates.Webapi/Program.cs
+++ Source_Summer/PlaylistMates/PlaylistMates.Webapi/Program.cs
@@ -65,6 +65,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials()); // allow credentials
+
 app.UseAuthentication();
 app.UseAuthorization();
 
